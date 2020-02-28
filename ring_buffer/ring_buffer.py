2e7029863e271b4cd0f7c016bf4195ab21ac18ac
Bayron Puac <bayronpuac@gmail.com>
2020-02-28 11:28:44 -0600

    Finished with Ring_buffer append

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,14 +8,28 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if self.current == self.capacity:
+             # This add the iteam to the tail
+            self.storage.add_to_tail(item)
+            # save current and changes pointers
+            cur_head = self.storage.head
+            # set head to the one that was next of head
+            self.storage.head = cur_head.next
+            # this allows pointer to point from head to tail
+            self.storage.head.prev = self.storage.tail
+             # this allows pointer to point from head to tail
+            self.storage.tail.next = self.storage.head
+        else:
+            self.storage.add_to_tail(item)
+            self.current += 1
+            
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
-
+        
         return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
